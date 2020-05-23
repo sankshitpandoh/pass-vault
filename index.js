ee5fdd72715f53a45454b73c2ef7e375fd25ad2c
sankshitpandoh <sankshit02pandoh.sp@gmail.com>
2020-05-23 18:21:11 +0530

    added login autho for client and configured server for the same

--- index.js
+++ index.js
@@ -1,10 +1,13 @@
 let express = require('express');
 const fs = require('fs');
+const bodyParser = require('body-parser');
+const crypto = require('crypto');
 let app = express();
 
 const port = process.env.PORT || 3000;
 
 app.use(express.static('public'));
+app.use(bodyParser.json({limit: '10mb', extended: true}));
 
 let server = app.listen(port, () => {
     let host = server.address().address;
@@ -12,3 +15,28 @@ let server = app.listen(port, () => {
 
     console.log("App running at http://%s:%s" , host, port)
 });
+
+/* API called when user attempts to log in */
+app.post("/logInAttempt" , function(req,res){
+    let uName = req.body.user;
+    let password = req.body.pass;
+    /* Hashing the password */
+    hashPwd = crypto.createHash('sha1').update(password).digest('hex');
+    let exist = false;
+    fs.readFile('./data/userInfo.json' , function(err, Data){
+        let dataArray = JSON.parse(Data);
+        /* Checking if the user exists */
+        for(let i = 0; i < dataArray.length; i++){
+            if(dataArray[i].userName === uName && dataArray[i].password === hashPwd){
+                exist = true;
+                break;
+            }
+        }        
+    });
+    if(exist === true){
+        res.send("true");
+    }
+    else{
+        res.send("false");
+    }
+})
--- public/assets/js/main.js
+++ public/assets/js/main.js
@@ -23,3 +23,44 @@ function loadSignUp(){
     request.open("GET", "./pages/signUp.html", true);
     request.send();
 }
+
+/* to check if fields are empty or not */
+const isEmpty = str => !str.trim().length;
+
+/* Function triggered when user wants to log in */
+function logIn(){
+    let uName = document.getElementById("user-name").value;
+    let password = document.getElementById("password").value;
+    if(isEmpty(uName)){
+        alert('User name field cannot be empty');
+        document.getElementById("user-name").value = "none";
+    }
+    else if(isEmpty(password)){
+        alert('Password field cannot be empty');
+        document.getElementById("password").value = "";
+    }
+    else{
+        /* object containing data that would be sent to server */
+        let logDetails = {
+            user : uName,
+            pass : password
+        }
+        console.log(logDetails)
+        let xhttp = new XMLHttpRequest();
+        xhttp.open("POST", "http://localhost:3000/logInAttempt" , true);
+        xhttp.setRequestHeader("Content-Type","application/json; charset=utf-8");
+        xhttp.send((JSON.stringify(logDetails)));
+        xhttp.onreadystatechange = function(){
+            if(this.readyState == 4 && this.status == 200){
+                if(this.response == "false"){
+                    alert("User doesn't exist, please try again or if a new user, sign Up for an account");
+                    document.getElementById("user-name").value = "";
+                    document.getElementById("password").value = "";
+                }
+                if(this.response == "true"){
+                    console.log("we going in homie");
+                }
+            }
+        }
+    }
+}
